419a068f6cde25dd5f10718a99b94a60257ef42e
Katie Greer <katieggreer@gmail.com>
2025-11-06 08:32:28 -0700

    FE for register/login, created popup-app.tsx to hold all components that go in the popup

---
+++ job_extension/src/chrome-extension/auth/Auth.tsx
@@ -0,0 +1,39 @@
+import './auth.css';
+interface Props{
+   // onEnterFunction: (param: React.KeyboardEvent<HTMLElement>) => void;
+    setPassword: (param: string) => void;
+    setUsername: (param: string) => void;
+    handleSubmit: (param: React.FormEvent) => void;
+    username: string;
+    password: string;
+
+}
+
+
+function Auth(props : Props){
+    return (
+    <form onSubmit={props.handleSubmit} className="form">
+        <input
+          type="username"
+          placeholder="username"
+          value={props.username}
+          onChange={(e) => props.setUsername(e.target.value)}
+          className="input"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={props.password}
+          onChange={(e) => props.setPassword(e.target.value)}
+          className="input"
+          required
+        />
+        <button type="submit" className="button">
+          Sign in
+        </button>
+      </form>
+  );
+}
+
+export default Auth;
--- job_extension/src/main.tsx
+++ job_extension/src/main.tsx
@@ -1,13 +1,20 @@
-import { StrictMode } from "react";
+//import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Popup } from "./chrome-extension/popup/index";
+//import { Popup } from "./chrome-extension/popup/index";
 import "./chrome-extension/global.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./popup-app";
+import { StrictMode } from "react";
+
 
 createRoot(document.getElementById("root")!).render(
+
   <StrictMode>
     {/* Small, compact container for the browser action popup */}
     <div className="bg-white w-[360px] max-h-[640px] p-4 rounded-md shadow-md">
-      <Popup />
+      <BrowserRouter>
+      <App />
+    </BrowserRouter>
     </div>
   </StrictMode>
 );
